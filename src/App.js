d0ebfc0e5a6f32689cfddcb19d67f7146907d7a0
3ddiehead <33770592+3ddiehead@users.noreply.github.com>
2022-07-02 12:53:49 -0700

    learned how css and svg work

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <div>
+        <button type="button" className="NavSandwich">
+          <svg width="24" height="17" viewBox="0 0 24 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="0.172974" width="20" height="3" rx="1.5" fill="#000000"></rect>
+            <rect x="0.172974" y="7" width="20" height="3" rx="1.5" fill="#000000"></rect>
+            <rect x="0.172974" y="14" width="20" height="3" rx="1.5" fill="#000000"></rect>
+            </svg>
+        </button>
+        <button>
+          INFO
+        </button>
+        <center>
+          Welcome to Squordle!
+        </center>
+        <button style={{textAlign: "right"}}>
+          OPTIONS
+        </button>
+      </div>
+    </header>
   );
 }
 
---
+++ src/NavBar.js
@@ -0,0 +1,12 @@
+function Navbar() {
+	return (
+
+	<div className="AppHeader-menuLeft">
+    	<button type="button" id="AppHeader-navsandwich" className="AppHeader-navsandwich">
+        </button>
+    </div>
+
+	);
+}
+
+export default NavBar;
